5b9f3ab025c375bbfd609ffe7f796469481b7f39
ThatCookie <xilefznokan@gmail.com>
2021-01-01 17:34:20 +0100

    FUCK YOU CSS

--- client/src/start.js
+++ client/src/start.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -8,7 +8,7 @@ function createWindow () {
     }
   })
 
-  win.loadFile('./html/index.html')
+  win.loadFile('./html/menu.html')
 }
 
 app.whenReady().then(createWindow)
@@ -17,6 +17,10 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
+})
+
+ipcMain.on('close-me', (evt, arg) => {
+app.quit()
 })
 
 app.on('activate', () => {
